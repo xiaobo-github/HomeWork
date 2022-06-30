62a5b9d76b0945561296a88c3bd841966125f0fb
orakcool <realjmail@mail.ru>
2022-06-30 17:53:59 +0300

    hw_1

---
+++ hw_1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mvn clean package
+
+java -jar target/hw.jar
---
+++ hw_1/src/main/java/com/orakcool/Main.java
@@ -0,0 +1,10 @@
+package com.alevel.base;
+
+import com.alevel.base.Core.Program;
+
+public class Main {
+
+    public static void main(String[] args){
+        Program.run();
+    }
+}
