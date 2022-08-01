c64a7dc1e9bfead73c8c3e18e67e18c926094be9
orakcool <realjmail@mail.ru>
2022-08-02 01:57:36 +0300

    hw_14

--- src/main/java/com/orakcool/hw_10/Main.java
+++ src/main/java/com/orakcool/hw_10/Main.java
@@ -1,9 +1,9 @@
 package com.orakcool.hw_10;
 
-import com.orakcool.hw_10.app.CRUD;
+import com.orakcool.hw_10.app.ListApp;
 
 public class Main {
     public static void main(String[] args) {
-        CRUD.run();
+        ListApp.run();
     }
 }
