0027e5d7183a4ebb9f4b5b1b02096d448866c757
orakcool <realjmail@mail.ru>
2022-08-03 00:22:15 +0300

    upd

--- src/main/java/com/orakcool/hw_10/util/ProductComparator.java
+++ src/main/java/com/orakcool/hw_10/util/ProductComparator.java
@@ -8,7 +8,7 @@ public class ProductComparator <T extends Product> implements Comparator<T> {
 
     @Override
     public int compare(T o1, T o2) {
-        double price = o1.getPrice() - o2.getPrice();
+        double price = o2.getPrice() - o1.getPrice();
         if (price == 0){
             int titleCompare = o1.getTitle().compareTo(o2.getTitle());
             if (titleCompare == 0){
